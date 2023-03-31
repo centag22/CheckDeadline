823755362ffd025b00adb79cbd276d92454d8a59
Eniola <techcentag@gmail.com>
2023-03-31 07:39:49 -0700

    checking time left for goal set on To-Do List

---
+++ set_deadline_for_to-do.py
@@ -0,0 +1,16 @@
+import datetime
+
+user_input=input("enter your goal with deadline separated by colon\n")
+input_list = user_input.split(":")
+# creating variables #
+goal = input_list[0]
+deadline = input_list[1]
+
+# converting python predefine string for all element to date format.converting string into datetime#
+deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+today_date = datetime.datetime.today()
+time_till = deadline_date - today_date
+# calculate how many days from now till deadline @
+# print(deadline_date - today_date) #
+# print(type(datetime.datetime.strptime(daedline, "%d.%m.%Y")))#
+print(f"Dear User ! Time remaining for your goal: {goal} is {time_till} days")
